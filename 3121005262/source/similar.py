9eea7e57611a3d8158092815b2f9ba5837bc6183
Glowww-s <101883501+Glowww-s@users.noreply.github.com>
2023-09-14 20:13:30 +0800

    refactor: change io of funtion vector_merge and cosine_similarity

--- 3121005262/source/similar.py
+++ 3121005262/source/similar.py
@@ -3,8 +3,8 @@ import numpy as np
 
 # 计算余弦相似度
 def cosine_similarity(doc_vectors):
-    a = np.array(list(doc_vectors[0].values()))
-    b = np.array(list(doc_vectors[1].values()))
+    a = doc_vectors[0]
+    b = doc_vectors[1]
     cos_sim = np.dot(a, b) / (np.linalg.norm(a) * np.linalg.norm(b))
     assert (0 <= cos_sim <= 1), "相似度的值超出上下限，算法出错。"
     return cos_sim
